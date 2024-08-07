652ce1854a158f02d2e4f1352e58a1c02b895ce6
asifaowadud <asifaowadud@gmail.com>
2024-08-07 23:36:42 +0600

    event details get done now edit envent phase

--- src/rtk/reducers/singleEventSlice.ts
+++ src/rtk/reducers/singleEventSlice.ts
@@ -1,39 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { fetchSingleEventAction } from "./singleEventAction";
+// import { createSlice } from "@reduxjs/toolkit";
+// import { fetchSingleEventAction } from "./singleEventAction";
 
-const initialState = {
-  event: {},
-  isLoading: false,
-  error: null,
-};
+// const initialState = {
+//   event: {},
+//   isLoading: false,
+//   error: null | string,
+// };
 
-const eventSlice = createSlice({
-  name: "event",
-  initialState,
-  reducers: {
-    clearSingleEventErrors: (state) => {
-      state.error = null;
-    },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(fetchSingleEventAction.pending, (state) => {
-      state.isLoading = true;
-    });
-    builder.addCase(fetchSingleEventAction.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.event = action.payload.event || {};
-      state.error = null;
-    });
-    builder.addCase(fetchSingleEventAction.rejected, (state, action) => {
-      state.isLoading = false;
-      state.event = {};
-      state.error = action.error.message || "Failed to fetch event";
-    });
-  },
-});
+// const eventSlice = createSlice({
+//   name: "event",
+//   initialState,
+//   reducers: {
+//     clearSingleEventErrors: (state) => {
+//       state.error = null;
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder.addCase(fetchSingleEventAction.pending, (state) => {
+//       state.isLoading = true;
+//     });
+//     builder.addCase(fetchSingleEventAction.fulfilled, (state, action) => {
+//       state.isLoading = false;
+//       state.event = action.payload.event || {};
+//       state.error = null;
+//     });
+//     builder.addCase(fetchSingleEventAction.rejected, (state, action) => {
+//       state.isLoading = false;
+//       state.event = {};
+//       state.error = action.error.message || "Failed to fetch event";
+//     });
+//   },
+// });
 
-export const { clearSingleEventErrors } = eventSlice.actions;
-export default eventSlice.reducer;
+// export const { clearSingleEventErrors } = eventSlice.actions;
+// export default eventSlice.reducer;
 
 // import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // import {
